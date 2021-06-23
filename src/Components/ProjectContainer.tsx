a38de5d230c7bea0a16bf0c35ff7a667811652c8
Noemi Puskas <amifontos@gmail.com>
2021-06-23 13:35:24 +0200

    Projects: Added flexbox

--- src/Components/ProjectContainer.tsx
+++ src/Components/ProjectContainer.tsx
@@ -5,6 +5,8 @@ let ProjectContainer = () => {
     <div className="project-container">
       <h1>Projects</h1>
       <Project projectThumbnail={null} projectViewLink={null} projectCodeLink={null} projectTechnologies={null} projectTools={null} />
+      <Project projectThumbnail={null} projectViewLink={null} projectCodeLink={null} projectTechnologies={null} projectTools={null} />
+      <Project projectThumbnail={null} projectViewLink={null} projectCodeLink={null} projectTechnologies={null} projectTools={null} />
     </div>
   );
 }
